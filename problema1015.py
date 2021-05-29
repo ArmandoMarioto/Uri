f2b77e7e7d49e0a0450fb5f44b945ea9849f69c5
Armando <armando_marioto100@hotmail.com>
2021-05-29 19:37:00 -0300

    mais resolvidos

---
+++ problema1015.py
@@ -0,0 +1,23 @@
+"""
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e
+calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+Distancia =
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: x1 y1 e
+ a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+Saída
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+"""
+
+x1, y1 = input().split(" ")
+x2, y2 = input().split(" ")
+x1 = float(x1)
+x2 = float(x2)
+y1 = float(y1)
+y2 = float(y2)
+distancia = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+print(f'{distancia:.4f}')
