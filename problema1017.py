0087026f40b2fd9ad16fa1c034eb77457fe53436
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-03 10:16:30 -0300

    resolvendo.

--- problema1017.py
+++ problema1017.py
@@ -12,7 +12,51 @@ O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viage
 Saída
 Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
 """
-tempo = int(input())
-media = int(input())
+# tempo = int(input())
+# media = int(input())
+#
+# print(f'{((tempo*media)/12):.3f}')
 
-print(f'{((tempo*media)/12):.3f}')
+# anterior = 0
+# proximo = 0
+# num = int(input())
+#
+# while(proximo <= num):
+#     print(proximo)
+#     proximo = proximo + anterior
+#     anterior = proximo - anterior
+#     if(proximo == 0):
+#         proximo = proximo + 1
+#
+# if(anterior == num):
+#     print('Número Pertence a Fibonacci')
+# else:
+#     print('O Número não pertence a Fibonacci')
+
+# import numpy
+#
+# numeros = numpy.array([5514, 6799, 89800, 0, 21.5])
+# num = numeros[numeros != 0]
+# print(f'O menor valor de faturamento ocorrido em um dia do ano é :{min(num)}')
+# print(f'O maior valor de faturamento ocorrido em um dia do ano : {max(num)}')
+# k = 0
+# media = sum(num) / (len(num))
+# for i in range(len(num)):
+#     if num[i] > media:
+#         k += 1
+#
+# print(f'Número de dias no ano em que o valor de faturamento diário foi superior à média anual é {k}')
+
+
+# SP = 67836.43
+# RJ = 36678.66
+# MG = 29229.88
+# ES = 27165.48
+# Outros = 19849.53
+# total = SP + RJ + MG + ES + Outros
+#
+# print(f'SP tev o percentual de {(SP*100)/total:.2f}')
+# print(f'RJ tev o percentual de {(RJ*100)/total:.2f}')
+# print(f'MG tev o percentual de {(MG*100)/total:.2f}')
+# print(f'ES tev o percentual de {(ES*100)/total:.2f}')
+# print(f'Outros tev o percentual de {(Outros*100)/total:.2f}')
